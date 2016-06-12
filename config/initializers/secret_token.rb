3f2cbc751490bb8b46f031341a119c6a9d2b3f1d
Natsuki Yamaguchi <natsuking@Natsuki-no-MacBook-Air.local>
2016-06-12 22:02:42 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shiz::Application.config.secret_key_base = 'c2b59afd64d880fb77bb86d0986d825f58ee7edcedc3cbdc8089442e68631c18780c8b28dd86fc862260fbe5a109fbdcaba4ff055a138d72e8562335cdc19b73'
